9ee06ddd35f46ee6f643f4959c235fa2f04e20e3
Liam Slade <liamslade2@gmail.com>
2025-08-07 09:49:10 -0400

    Initialized main function. Implemented parser and added arguments to pass. Also added additional defaults in config file.

--- config.py
+++ config.py
@@ -1,1 +1,18 @@
-DEFAULT_COLUMN = "PDF Filename"
+# Default column to write to
+DEFAULT_COLUMN = "A"
+
+# Default Excel file name if none is provided
+DEFAULT_EXCEL_FILENAME = "file_names.xlsx"
+
+# Default sheet name (only used if none is passed)
+DEFAULT_SHEET_NAME = "File Names"
+
+# Default header text for the column 
+DEFAULT_HEADER = "PDF Filenames"
+
+# Whether to include hidden files
+INCLUDE_HIDDEN_FILES = False
+
+# Whether to inlcude full paths instead of just names
+USE_FULL_PATH = False
+
--- filename_to_excel.py
+++ filename_to_excel.py
@@ -4,3 +4,36 @@ import openpyxl
 import argparse
 from config import DEFAULT_COLUMN
 
+def main():
+    # Initialize argument parser
+    parser = argparse.ArgumentParser(description="Read file names from a given directory, and copy them to an Excel file.")
+
+    # Add arguments for agrument parser
+    parser.add_argument(
+        "directory",
+        type=str,
+        help="Path to the directory containing files."
+    )
+    parser.add_argument(
+        "--output", 
+        type=str,
+        help='Path to the Excel file to write to. If not specified, a new Excel file named "file_names.xlsx" will be created in the directory.',
+        default=None
+    )
+    parser.add_argument(
+        "--sheet",
+        type=str,
+        help='Name of the sheet to write to. Default is the first (active) sheet.',
+        default=None
+    )
+    parser.add_argument(
+        "--column",
+        type=str,
+        help=f'Column letter to insert file names into. Default is "{DEFAULT_COLUMN}".',
+        default=DEFAULT_COLUMN
+    )
+    parser.add_argument(
+        "--append",
+        action="store_true",
+        help="If set, file names will be appended to the existing Excel file (if it exists). Otherwise, it will overwrite or create a new one."
+    )
